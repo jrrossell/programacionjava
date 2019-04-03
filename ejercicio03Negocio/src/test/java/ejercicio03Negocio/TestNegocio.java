9ab2891daf6476fef5fde0be5e8b851a9a2eab71
jrrossell <junior.rossell@gmail.com>
2019-04-03 14:00:04 +0200

    Test completos. - Inicio MVC

--- ejercicio03Negocio/src/test/java/ejercicio03Negocio/TestNegocio.java
+++ ejercicio03Negocio/src/test/java/ejercicio03Negocio/TestNegocio.java
@@ -2,6 +2,9 @@ package ejercicio03Negocio;
 
 import static org.junit.Assert.*;
 
+import java.util.HashSet;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,9 +13,9 @@ import com.curso.java.oo.ejercicio01oo.model.Aula;
 import com.curso.java.oo.ejercicio01oo.model.PuestoDeTrabajo;
 
 public class TestNegocio {
-	
+
 	GestionDeAulas negocioAulas = new GestionDeAulas();
-	
+
 	@Before
 	public void setUp() throws Exception {
 	}
@@ -22,20 +25,20 @@ public class TestNegocio {
 		Aula aula = new Aula();
 		aula.setNombre("Kepler");
 		negocioAulas.registrarAula(aula);
-		
+
 		boolean existeAula = false;
 		if (negocioAulas.getAula().size() >= 1) {
 			existeAula = true;
 		}
-		assertTrue("Se registro el aula", existeAula);
+		assertTrue("No se registro el aula", existeAula);
 	}
-	
+
 	@Test
 	public void listaAulasNegocio() {
 		Aula aula = new Aula();
 		aula.setNombre("Kepler");
 		negocioAulas.registrarAula(aula);
-		
+
 		assertFalse("Lista de aulas vacia", negocioAulas.getAula().isEmpty());
 	}
 
@@ -46,7 +49,7 @@ public class TestNegocio {
 		negocioAulas.registrarAula(aula);
 		aula.setNombre("Kepler");
 		negocioAulas.modificarAula(aula);
-		
+
 		boolean existeAula = false;
 		for (Aula siguienteAula : negocioAulas.getAula()) {
 			if (siguienteAula.getNombre().equals("Kepler")) {
@@ -54,37 +57,67 @@ public class TestNegocio {
 				break;
 			}
 		}
-		assertTrue("Se modifico el aula", existeAula);
+		assertTrue("No Se modifico el aula", existeAula);
 	}
-	
+
 	@Test
 	public void eliminarAulaNegocio() {
 		Aula aula = new Aula();
 		aula.setNombre("Galileo");
 //		negocioAulas.registrarAula(aula);
 		negocioAulas.eliminarAula("Galileo");
-		
+
 		boolean existeAula = false;
 		if (negocioAulas.getAula().size() == 0) {
 			existeAula = true;
 		}
-		assertTrue("Se elimino el aula", existeAula);
+		assertTrue("No Se elimino el aula", existeAula);
 	}
-	
+
 	@Test
 	public void asignarAlumnosPorAulaNegocio() {
 		PuestoDeTrabajo puesto = new PuestoDeTrabajo();
 		puesto.setOrdenador(true);
 
 		Aula aula = new Aula();
+		aula.setPuestosDeAlumnos(new HashSet<PuestoDeTrabajo>());
 		aula.setNombre("Galileo");
 		aula.getPuestosDeAlumnos().add(puesto);
-		
+		negocioAulas.registrarAula(aula);
+
 		Alumno alumno = new Alumno();
 		alumno.setNombre("Junior");
-		
+
 		negocioAulas.asignarAlumnoAlAula(alumno, aula);
-		
-		assertFalse("Lista de aulas vacia", negocioAulas.getAula().isEmpty());
+
+		boolean existeAula = false;
+		if (negocioAulas.getAula().size() >= 1) {
+			existeAula = true;
+		}
+		assertTrue("Aula vacia", existeAula);
+	}
+
+	@Test
+	public void listarAlumnosPorAulaNegocio() {
+		PuestoDeTrabajo puesto = new PuestoDeTrabajo();
+		puesto.setOrdenador(true);
+
+		Aula aula = new Aula();
+		aula.setPuestosDeAlumnos(new HashSet<PuestoDeTrabajo>());
+		aula.setNombre("Galileo");
+		aula.getPuestosDeAlumnos().add(puesto);
+		negocioAulas.registrarAula(aula);
+
+		Alumno alumno = new Alumno();
+		alumno.setNombre("Junior");
+
+		negocioAulas.asignarAlumnoAlAula(alumno, aula);
+
+		boolean existenAlunmos = false;
+		List<Alumno> alumnos = (List<Alumno>) negocioAulas.listaDeAlumnoPorAula("Galileo");
+		if (alumnos.size() >= 1) {
+			existenAlunmos = true;
+		}
+		assertTrue("Aula sin alumnos", existenAlunmos);
 	}
 }
